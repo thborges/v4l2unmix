c21ed89c0e5dda8b2183c6c852413098a58d5982
thborges <thborges@gmail.com>
2019-08-30 17:01:23 -0300

    Adding filters to the web interface

--- utils/motion_view.php
+++ utils/motion_view.php
@@ -26,22 +26,54 @@ echo "<html>
 			}
 		}
 		</script>
-	<body\r";
+	<body>\r";
 
+# events for the day
 if ($_GET['day'] != null && $_GET['monitor'] != null) {
+
 	$m = $_GET['monitor'];
 	$d = $_GET['day'];
-	$events = glob("./$dir/$m/$d/*.jpg");
+	$df = $_GET['dayfrag'];
+
+	# day splits, add more if you need!
+	$daysplt = array([0,6], [6,12], [12,18], [18,24]);
+
+	$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+	foreach($daysplt as $k => $v) {
+		echo "<a href='$path?monitor=$m&day=$d&dayfrag=$k'>$v[0]h to $v[1]h</a>";
+	}
+	echo "<br>";
+
+	$eventsize = 0;
+	$count = array();
+	$events = glob("$dir/$m/$d/*.jpg");
 	$e = end($events);
 	do {
 		if ($e == '.' || $e == '..') continue;
-		#echo "<a target='popup' href='./$dir/$m/$d/$e/$e-video.mp4'><img width='200px' src='./$dir/$m/$d/$e/snapshot.jpg'></img></a>";
-		$v = substr_replace($e, "mp4", -3);
-		echo "<img id='$e' width='200px' src='$e'
-			onclick='showvideo(\"v$e\", \"$v\")'></img><div id='v$e' style='display: none;'></div>";
 
+		$fh = date('H', filemtime($e));
+		if ($df == null || ($fh >= $daysplt[$df][0] && $fh < $daysplt[$df][1])) {
+			$v = substr_replace($e, "mp4", -3);
+			$id = basename($e, '.jpg');
+			echo "<img width='200px' src='$e'
+				onclick='showvideo(\"v$id\", \"$v\")'></img>
+			      <div id='v$id' style='display: none;'></div>";
+			$count[$fh] = ($count[$fh] ?? 0) + 1;
+		}
+		$eventsize += filesize($e);
+		$eventsize += filesize($v);
 	} while ($e = prev($events));
+
+	echo "<br><h2>Histogram of events, hour:number</h2>";
+	$t = 0;
+	foreach($count as $k => $v) {
+		echo "${k}h: $v<br>";
+		$t += $v;
+	}
+	echo "total: $t events, " . number_format($eventsize/1024/1024/1024, 2) . " GB";
 }
+
+# main page
 else {
 	echo "<a href='/cam1'> <img src='/cam1' border=0 width=25%></a>
 	      <a href='/cam2'> <img src='/cam2' border=0 width=25%></a>";
